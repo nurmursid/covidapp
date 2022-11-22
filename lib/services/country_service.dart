810f50da7cb797abaa04647e11e8688e2eb861b3
nurmursid <iqbalnurmursid@gmail.com>
2022-11-22 23:36:13 +0700

    baseurl

--- lib/services/country_service.dart
+++ lib/services/country_service.dart
@@ -5,7 +5,7 @@ import 'package:covidapp/models/country_model.dart';
 
 class CountryService {
   String baseUrl =
-      'https://my-json-server.typicode.com/cinkasihaloho/tugasakhirmdp_covidapp/country';
+      'https://my-json-server.typicode.com/nurmursid/provinsi/country';
 
   Future<List<Country>> getCountry() async {
     var url = Uri.parse(baseUrl);
